ef435509569484ac6b3a876f09d9edcf5f9fffba
shigekix98 <mth.lv.42@gmail.com>
2026-02-07 06:40:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -177,7 +177,7 @@ if len(view) > 0:
             name="試合数",
             opacity=0.1,
             yaxis="y2",
-            marker_color="orange"
+            marker_color="white"
         )
     )
 
